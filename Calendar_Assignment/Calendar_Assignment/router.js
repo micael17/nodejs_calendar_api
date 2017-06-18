b406a15a13414552809a783c09bd1d4475880dd6
Jihong Kim <micael17@naver.com>
2017-06-18 08:15:27 +0000

    개인 일정 로직 1차 완성

--- Calendar_Assignment/Calendar_Assignment/router.js
+++ Calendar_Assignment/Calendar_Assignment/router.js
@@ -11,10 +11,10 @@ module.exports = function (app, moment, dateChker) {
     var dateChker = require('./dateChker');
     
     
-    // Ȩ ȭ��(���� ���� Ȯ��)
+    // 홈 화면(오늘 일정 확인)
     app.get('/calendar', function (req, res) {
 
-        //���� ��¥�� JSON ���·� ��ȯ�Ѵ�.
+        //오늘 날짜를 JSON 형태로 반환한다.
 
         var date = {
             "year": moment().format('YYYY'),
@@ -27,50 +27,123 @@ module.exports = function (app, moment, dateChker) {
         res.send(parsedDate);
     });
     
+    // 년별 캘린더 확인       
+    app.get('/calendar/year', function (req, res) {
+    //1월부터 12월까지만 받는다. 그 외 숫자는 error로 응답한다. 
+    //##프론트엔드에서 해당 월의 시작 요일 처리. 백엔드에서는 해당 월의 전체 스케쥴 반환
+        var year = req.query.year;
 
-    // ���� Ķ���� Ȯ��       
-    app.get('/calendar/month', function (req, res) {
-    //1������ 12�������� �޴´�. �� �� ���ڴ� error�� �����Ѵ�. 
-    //##����Ʈ���忡�� �ش� ���� ���� ���� ó��. �鿣�忡���� �ش� ���� ��ü ������ ��ȯ
-        var month = req.query.month;
-        var chk = dateChker.monthChk(month);
-        if (chk) {
-            if (month < 10) {
-                month = '0' + month;
-            }            
-
-            //��¥ ������ �����ϴ� �ɼ�
+            //날짜 순으로 정렬하는 옵션
             var options = {
                 sort: {
                     date: 1
                 }
             };
-
-            PrivateCalSchema.find({ date: { "$gte": new Date("2017-" + month + "-01T00:00:00.000Z"), "$lte": new Date("2017-" + month + "-31T23:59:59.000Z") } }, null, options, function (err, data) {
+        PrivateCalSchema.find({ start_time: { "$gte": new Date(year), "$lte": new Date(year + '-12-31') } }, null, options, 
+            function (err, data) {
                 if (data != null) {
                     res.send(data);
                 } else {
                     res.send('err');
                 }
             });
+    });
+
+    // 월별 캘린더 확인       
+    app.get('/calendar/month', function (req, res) {
+    //1월부터 12월까지만 받는다. 그 외 숫자는 error로 응답한다. 
+    //##프론트엔드에서 해당 월의 시작 요일 처리. 백엔드에서는 해당 월의 전체 스케쥴 반환
+        
+        var year = req.query.year;
+        var month = req.query.month;
+        
+        //파라미터 확인
+        if(year == null){
+            res.send('year is null');
+            return;
+        }else if(month == null){
+            res.send('month is null');
+            return;
+        }
+        
+        //해당 월과 다음 월 세팅(Date 쿼리를 위해 세팅한다.)
+        if(Number(month) >= 12){
+            var nextMonth = 1;
+        }else{
+            var nextMonth = Number(month)+1;
+        }
+        
+        
+        var chk = dateChker.monthChk(month);
+        if (chk) {
+            if (month < 10) {
+                month = '0' + month;
+            }            
+            
+            if(nextMonth < 10){
+                nextMonth = '0'+nextMonth;
+            }
+
+            //날짜 순으로 정렬하는 옵션
+            var options = {
+                sort: {
+                    start_time: 1
+                }
+            };
+            console.log(month);
+            console.log(nextMonth);
+            
+            var nextYear;
+            if(nextMonth < month){
+                nextYear = 2018;
+            }else{
+                nextYear = 2017;
+            }
+            
+            PrivateCalSchema.find({start_time: { "$gte": new Date(year + '-' + month), "$lte" : new Date(nextYear + '-' + nextMonth)}}, null, options, function (err, data) {
+                if (data != null) {
+                    res.send(data);
+                } else {
+                    res.send('err');
+                }
+            });
+            
         } else {
             res.send('error');
         }
     });
+    
+    
 
-    // �ش� ��¥(��) ���� ����Ʈ Ȯ��
+    // 해당 날짜(일) 일정 리스트 확인
     app.get('/calendar/chkSchedule', function (req, res) {
+        //프론트엔드에서는 편하게 숫자로만 받는다. 
+        //ex) 3, 14 (3월 14일)
+        //백엔드에서 형식을 맞춰준다.
+        
+        var year = req.query.year;
         var month = req.query.month;
         var day = req.query.day; 
+        
+        //파라미터 확인
+        if(year == null){
+            res.send('year is null');
+            return; 
+        }else if(month == null){
+            res.send('month is null');
+            return;
+        }else if(day == null){
+            res.send('day is null');
+            return;
+        }
 
-        //�÷�ID ������ �����ϴ� �ɼ�
+        //컬럼ID 순으로 정렬하는 옵션
         var options = {
 	        sort : {
                 _id: 1
 	        }	
         };
-        console.log('test');
-        console.log(month, day);
+        
         var chk = dateChker.monthChk(month, day);
     
         if (chk) {
@@ -82,7 +155,7 @@ module.exports = function (app, moment, dateChker) {
                 day = '0'+day;
             }
 
-            PrivateCalSchema.find({date : {"$gte": new Date("2017-"+month+"-"+day+"T00:00:00.000Z"),"$lte": new Date("2017-"+month+"-"+day+"T24:00:00.000Z")}}, null, options, function(err, data){
+            PrivateCalSchema.find({start_time : {"$gte": new Date(year+"-"+month+"-"+day+"T00:00:00.000Z"),"$lte": new Date(year+"-"+month+"-"+day+"T23:59:59.000Z")}}, null, options, function(err, data){
 			    if(data != null){
 				    res.send(data);
 			    }else{
@@ -95,24 +168,84 @@ module.exports = function (app, moment, dateChker) {
 
     });
 
-    // ���� ���� �߰�
+    // 개인 일정 추가
     app.post('/calendar/addPrvSchedule', function (req, res) {
+        //프론트엔드에서는 편하게 숫자 또는 문자로만 받는다. 
+        //ex) 3, 14, 13:00, 15:00 (3월 14일 13시00분 15시00분. 초 단위는 생략한다.)
+        
+        var year = req.body.year; 
         var month = req.body.month;
         var day = req.body.day;
         var startTime = req.body.startTime;
-        var endTime = req.body.endTime;        
+        var endTime = req.body.endTime;
         var schedule = req.body.schedule;
         var isBirthday = req.body.isBirthday;
         
+         //파라미터 확인
+         switch(true){
+            case (year == null) :
+                res.send('year is null');
+                return;
+            case (month == null) :
+                res.send('month is null');
+                return;
+            case (day == null) :
+                res.send('month is null');
+                return;
+            case (startTime == null):
+                res.send('startTime is null');
+                return;
+            case (endTime == null):
+                console.log("switch");
+                res.send('endTime is null');
+                return;
+            case (schedule == null):
+                res.send('schedule is null');
+                return;
+            case (isBirthday == null):
+                res.send('isBirthday is null');
+                return;
+         }
+                
+            
+        
+        
+        var startArray = startTime.split(':');
+        var endArray = endTime.split(':');
+        
+        if(Number(endArray[0]) < Number(startArray[0])){
+            res.send('error. 일정의 마감시간은 시작시간보다 빠를 수 없습니다.');
+            return;
+        }
+        /* 
+        if(Number(startArray[0]) > 23){
+            res.send('error. 시작시간은 최대 23시부터 가능합니다.');
+        }
+        */
+        
+        if(month < 10){
+            month = '0'+month;
+        }
+
+        if(day < 10){
+            day = '0'+day;
+        }
+        
+         //프론트에서 받아온 데이터는 백엔드에서 ISODate 형식으로 맞춰준다. 초 단위는 생략한다.
+        var modStartTime = year+'-'+month+'-'+day+'T'+startTime+':00.000Z';
+        var modEndTime = year+'-'+month+'-'+day+'T'+endTime+':00.000Z';
+        
         var chk = dateChker.monthDayChk(month, day);
-
-
+        
+        console.log(modEndTime);
+        
         if (chk) {
-            if(isBirthday == true){            
-                privateCalSchema = new PrivateCalSchema({user_idx : '1', date : new Date(2017, month - 1 , day + 1), schedule : 'My Birthday', isBirthday : true});
+            if(isBirthday == true){
+                //생일일 경우 boolean으로 생일 여부를 저장한다.
+                var privateCalSchema = new PrivateCalSchema({user_idx : '1', start_time : new Date(modStartTime), end_time : new Date(modEndTime), schedule : schedule, isBirthday : true});
                 privateCalSchema.save();
             }else{
-                privateCalSchema = new PrivateCalSchema({user_idx : '1', date : new Date(2017, month - 1 , day + 1), schedule : schedule, isBirthday : false});
+                var privateCalSchema = new PrivateCalSchema({user_idx : '1', start_time : new Date(modStartTime), end_time : new Date(modEndTime), schedule : schedule, isBirthday : false});
                 privateCalSchema.save();
             }
         } else {
@@ -122,12 +255,12 @@ module.exports = function (app, moment, dateChker) {
         res.end();
     });
 
-    // ���� ���� ����
+    // 개인 일정 삭제
     app.delete('/calendar/delPrvSchedule', function (req, res) {
 
-        //���� ������ ���� ���� ����Ʈ�� Ȯ���� ��, ����Ʈ �� �ϳ��� �����Ͽ� �����ϴ� ���丮�� �����Ѵ�.
-        //���� Ȯ�� ��, ����Ʈ���忡 �ش� �������� ���� id�� ���� �Ѱ�����.
-        //���� ��, �÷��� ID�� �����Ѵ�.
+        //일정 삭제는 먼저 일정 리스트를 확인한 후, 리스트 중 하나를 선택하여 삭제하는 스토리로 가정한다.
+        //일정 확인 시, 프론트엔드에 해당 일정들의 고유 id도 같이 넘겨진다.
+        //삭제 시, 해당 일정의 고유 ID로 삭제한다.
         
         var id = req.body.id;
 
@@ -142,23 +275,90 @@ module.exports = function (app, moment, dateChker) {
         
     });
 
-    // ���� ���� ����
+    // 개인 일정 수정
     app.put('/calendar/modPrvSchedule', function (req, res) {
 
-        //���� ������ ���� ���� ����Ʈ�� Ȯ���� ��, ����Ʈ �� �ϳ��� �����Ͽ� �����ϴ� ���丮�� �����Ѵ�.
-        //���� Ȯ�� ��, ����Ʈ���忡 �ش� �������� ���� id�� ���� �Ѱ�����.
-        //���� ��, �÷��� ID�� ��ȸ�Ͽ� �����Ѵ�.
+        //일정 수정은 먼저 일정 리스트를 확인한 후, 리스트 중 하나를 선택하여 수정하는 스토리로 가정한다.
+        //일정 확인 시, 프론트엔드에 해당 일정들의 고유 id도 같이 넘겨진다.
+        //수정 시, 컬럼의 ID로 조회하여 수정한다.
 
         var id = req.body.id;
         var reSchedule = req.body.schedule;
-        PrivateCalSchema.findByIdAndUpdate(id, { schedule : reSchedule }, function (err) {
-            if (!err) {
-                res.send('modify success');
-            }
-            else {
-                res.send('error');
-            }
-        });
+        var year = req.body.year; 
+        var month = req.body.month;
+        var day = req.body.day;
+        var startTime = req.body.startTime;
+        var endTime = req.body.endTime;    
+        var isBirthday = req.body.isBirthday;
+        
+        //파라미터 확인
+         switch(true){
+             case (id == null) :
+                res.send('id is null');
+                return;
+            case (year == null) :
+                res.send('year is null');
+                return;
+            case (month == null) :
+                res.send('month is null');
+                return;
+            case (day == null) :
+                res.send('month is null');
+                return;
+            case (startTime == null):
+                res.send('startTime is null');
+                return;
+            case (endTime == null):
+                console.log("switch");
+                res.send('endTime is null');
+                return;
+            case (reSchedule == null):
+                res.send('schedule is null');
+                return;
+            case (isBirthday == null):
+                res.send('isBirthday is null');
+                return;
+         }
+        
+        var startArray = startTime.split(':');
+        var endArray = endTime.split(':');
+        if(Number(endArray[0]) < Number(startArray[0])){
+            res.send('error. 일정의 마감시간은 시작시간보다 빠를 수 없습니다.');
+            return;
+        }
+        
+        if(month < 10){
+            month = '0'+month;
+        }
 
+        if(day < 10){
+            day = '0'+day;
+        }
+        
+        //프론트에서 받아온 데이터는 백엔드에서 ISODate 형식으로 맞춰준다. 초 단위는 생략한다.
+        var modStartTime = year+'-'+month+'-'+day+'T'+startTime+':00.000Z';
+        var modEndTime = year+'-'+month+'-'+day+'T'+endTime+':00.000Z';
+        
+        if(isBirthday == true){
+            PrivateCalSchema.findByIdAndUpdate(id, { schedule : reSchedule,  start_time : new Date(modStartTime), end_time : new Date(modEndTime), isBirthday : true }, 
+            function (err) {
+                 if (!err) {
+                    res.send('modify success');
+                }
+                else {
+                    res.send('error');
+                }
+            });
+        }else{
+            PrivateCalSchema.findByIdAndUpdate(id, { schedule : reSchedule, start_time : new Date(modStartTime), end_time : new Date(modEndTime) }, 
+            function (err) {
+                 if (!err) {
+                    res.send('modify success');
+                }
+                else {
+                    res.send('error');
+                }
+            });
+        }
     });
 }
--- Calendar_Assignment/Calendar_Assignment/routerTest.js
+++ Calendar_Assignment/Calendar_Assignment/routerTest.js
@@ -1,9 +1,44 @@
-var supertest = require("supertest");
-var should = require("should");
+//server.js 내용 가져옴 
+
+// [LOAD PACKAGES]
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+
+// [CONFIGURE APP TO USE bodyParser]
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// [CONFIGURE SERVER PORT]
+var port = process.env.PORT || 8585;
+
+// [CONFIGURE ROUTER]
+var router = require('./router')(app);
+
+// [RUN SERVER]
+var server = app.listen(port, function () {
+    console.log("Express server has started on port " + port)
+});
+
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function () {
+    // CONNECTED TO MONGODB SERVER
+    console.log("Connected to mongod server");
+});
+
+mongoose.connect('mongodb://localhost/test');
+
+/*
+ * 아래부터 테스트 코드
+ */
+var request = require("supertest");
+//var should = require("should");
 
 // This agent refers to PORT where program is runninng.
-
-var server = supertest.agent("http://localhost:8080");
 
 // UNIT test begin
 
@@ -12,19 +47,15 @@ describe("SAMPLE unit test",function(){
   // #1 should return home page
 
   it("should return home page",function(done){
-
-    // calling home page api
-    server
-    .get("/calendar")
-    .expect("Content-type",/json/)
-    .expect(200) // THis is HTTP response
-    .end(function(err,res){
-      // HTTP status should be 200
-      res.status.should.equal(200);
-      // Error key should be false.
-      res.body.error.should.equal(false);
-      done();
-    });
+      // calling home page api
+      request(app)
+          .get("/calendar")
+        //.expect("Content-type",/json/)
+        //.expect(200) // THis is HTTP response
+        .end(function (err, res) {
+            console.log(res.text);
+            done();
+         });
   });
 
 });
